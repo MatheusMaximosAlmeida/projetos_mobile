fe2fc894924bbace324014e508768b700ddf151f
Matheus Maximos Almeida O. Macedo <88410053+MatheusMaximosAlmeida@users.noreply.github.com>
2022-08-26 22:22:08 -0300

    ...

--- projeto_ClimaTempo/src/app/home/home.page.ts
+++ projeto_ClimaTempo/src/app/home/home.page.ts
@@ -1,4 +1,9 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { ResultFunc } from 'rxjs/internal/observable/generate';
+import { Forecast } from '../class/forecast';
+import { Results } from '../class/results';
+
 
 @Component({
   selector: 'app-home',
@@ -6,7 +11,17 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  results: Results  = new Results();
 
-  constructor() {}
+  constructor(private http: HttpClient) {
+    this.results.forecast = [new Forecast()];
+  }
 
+  ngOnInit(){
+    const cors = 'http://cors-anywhere.herokuapp.com/';
+    const url = cors + 'https://api.hgbrasil.com/weather?woeid=457351';
+    this.http.get(url).subscribe(
+      response => this.results = response["results"]
+    );
+  }
 }
