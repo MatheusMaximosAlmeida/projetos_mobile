7a273389fc01ac1cd041f6bf48cf726a6438b96a
Matheus Maximos Almeida O. Macedo <matheusmaximos452@gmail.com>
2022-10-25 22:42:35 -0300

    Login com Firebase
    Aula sobre implementação de login com Firebase.

---
+++ autenticacao/src/environments/environment.ts
@@ -0,0 +1,24 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false, 
+  firebaseConfig: {
+    apiKey: "AIzaSyDl8EXMj4siHPCSl5DSKT8UroS6TyDUJPQ",
+    authDomain: "autenticacao-ddd99.firebaseapp.com",
+    projectId: "autenticacao-ddd99",
+    storageBucket: "autenticacao-ddd99.appspot.com",
+    messagingSenderId: "48956791890",
+    appId: "1:48956791890:web:8521fe72cdb1723ebd938b"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
